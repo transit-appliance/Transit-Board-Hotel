7e724c3171a07fa74ba7af49837ba34d69bde9ed
Matt Conway <mattwigway@gmail.com>
2011-10-12 22:39:52 -0700

    Set default style back to 997.

--- tbdhotel.js
+++ tbdhotel.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
     // config section
     // it'd be better if the key was left as is, so that we can track traffic
     // style #46283 is the one Matt built for this project
-    var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/46283/256/{z}/{x}/{y}.png';
+    var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/997/256/{z}/{x}/{y}.png';
     var tileAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 
 
     var baseLayer = new L.TileLayer(tileUrl, 
 				    {maxZoom: 18, attribution: tileAttr});
+
+    var transitLayer = new L.TileLayer("http://localhost:8000/gis/trimetTiles/{z}/{x}/{y}.png",
+				       {maxZoom: 18});
+    
     var map = new L.Map(
 	'map',
 	{zoomControl: false})
-	.setView(new L.LatLng(45.5240, -122.6810), 14)
-	.addLayer(baseLayer);
+	.setView(new L.LatLng(45.5240, -122.6810), 0)
+	.addLayer(baseLayer)
+	.addLayer(transitLayer);
 });
