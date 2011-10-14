d6034bb128f7687e4eb63f5a6d527c2cec73c6af
Matt Conway <mattwigway@gmail.com>
2011-10-14 16:12:40 -0700

    Reworded attribution.

--- mapView.js
+++ mapView.js
@@ -15,13 +15,13 @@ $(document).ready(function () {
     // it'd be better if the key was left as is, so that we can track traffic
     // style #46244 is the one Matt built for this project
     var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/' + mapId + '/256/{z}/{x}/{y}.png';
-    var tileAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+    var tileAttr = 'Basemap data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 
     var baseLayer = new L.TileLayer(tileUrl, 
 				    {maxZoom: 18, attribution: tileAttr});
 
     var transitLayer = new L.TileLayer("gis/trimetTiles/{z}/{x}/{y}.png",
-				       {maxZoom: 18, attribution: 'Data courtesy TriMet'});
+				       {maxZoom: 18, attribution: 'Transit data courtesy TriMet'});
     
 
     var lat = query.lat?Number(query.lat):45.5240;
