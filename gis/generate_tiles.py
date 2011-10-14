97ceaaa62150f0054137c2da44984f138bb8ec9d
mattwigway <mattwigway@gmail.com>
2011-10-14 11:13:32 -0700

    License notes.

--- gis/generate_tiles.py
+++ gis/generate_tiles.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+# I'm not sure what the original license on this file is (it wasn't noted), so I am assuming it was
+# GPL2 like the rest of OpenStreetMap
+# Therefore, we can't re-license under apache2, so:
+
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 # Borrowed from OpenStreetMap SVN: 
 # http://svn.openstreetmap.org/applications/rendering/mapnik
 
