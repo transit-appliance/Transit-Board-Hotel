a97ab447e4d426f426f28b9e594af03fa919ce6f
Matt Conway <mattwigway@gmail.com>
2011-10-13 19:44:52 -0700

    Set back to style 46244.

--- tbdhotel.js
+++ tbdhotel.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // config section
     // it'd be better if the key was left as is, so that we can track traffic
-    // style #46283 is the one Matt built for this project
-    var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/997/256/{z}/{x}/{y}.png';
+    // style #46244 is the one Matt built for this project
+    var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/46244/256/{z}/{x}/{y}.png';
     var tileAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 
 
