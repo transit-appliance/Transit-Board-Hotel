b721fe1541d1317f2a23efb540f35e2fadd70bf1
Matt Conway <mattwigway@gmail.com>
2011-10-15 00:17:32 -0700

    Having some issues with the couch multiple document API, cross-origin requests, and JSON-P.

--- tbdhotel.js
+++ tbdhotel.js
@@ -1,4 +1,6 @@
 var realTimeArrivals;
+if (console.log != undefined) var log = console.log;
+else var log = function (m) {};
 
 $(document).ready(function () {
     trArr({
@@ -55,7 +57,7 @@ function tbdHotel() {
 	var tileUrl = 'http://{s}.tile.cloudmade.com/2d634343963a4426b126ab70b62bba2a/46244/256/{z}/{x}/{y}.png';
 	
 	var tileAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
-    }
+    }    
 	
     var rights_string = '';
     for (var agency in realTimeArrivals.stopsConfig) {
@@ -77,4 +79,24 @@ function tbdHotel() {
 	.setView(new L.LatLng(45.5240, -122.6810), 14)
 	.addLayer(baseLayer)
 	.addLayer(transitLayer);
+
+
+    // parse out the destinations
+    var destIds = realTimeArrivals.optionsConfig.destinations[0].split(',');
+    console.log('destinations: ' + destIds.join(' '));
+
+    // Use the Couch Multiple Document Interface to fetch all of the
+    // destinations in one fell swoop
+    $.ajax({
+	// include the documents, get them all
+	url: 'http://transitappliance.couchone.com/destinations/_all_docs?include_docs=true',
+	dataType: 'jsonp',
+	type: 'GET',
+	// filter to just the IDs we want
+	data: {keys: destIds},
+	success: function (data) {
+	    console.log('Successfully retrieved data');
+	}
+    });
 }
+
