66d59ea2b978ca2177e6d3899a12d69b142c8fae
Matt Conway <mattwigway@gmail.com>
2011-10-21 22:43:11 -0700

    First shot at a rails-based mobile app, eventually hosted on Heroku.

---
+++ mobile/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobile::Application.config.secret_token = 'efd2b4f639eff3dc856b62ec0cd11d77e8d91d8759ff1a3c66c02119132413be10286e99a7c7c2d19cb030df517187c419256b9c62aa7c7f9fa929826d185e4d'
