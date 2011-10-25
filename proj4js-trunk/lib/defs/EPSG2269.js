33d79dd42080ecf509e38a983270f02811dbce21
Matt Conway <mattwigway@gmail.com>
2011-10-24 22:54:36 -0700

    Custom build

---
+++ proj4js-trunk/lib/defs/EPSG2269.js
@@ -0,0 +1,2 @@
+// Oregon State Plane North, US Survey Feet
+Proj4js.defs["EPSG:2269"] = "+proj=lcc +lat_1=46 +lat_2=44.33333333333334 +lat_0=43.66666666666666 +lon_0=-120.5 +x_0=2500000.0001424 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=ft +no_defs";
---
+++ proj4js-trunk/lib/defs/EPSG4326.js
@@ -0,0 +1,2 @@
+// WGS 84
+Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0";
