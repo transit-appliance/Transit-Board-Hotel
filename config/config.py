b5b1787527fe88ec3fc7c537410e84508c2cb1e8
Matt Conway <mattwigway@gmail.com>
2011-11-18 00:11:29 -0800

    Significant progress on the config tool - now there is a cool AJAXy selector for destinations, which needs some CSS love but works otherwise. Also, I need to read current destinations and deal with the map <-> destination interaction (the center currently does not respond to the map changing).
    But not bad for 2 hours of effort in the very late evening/early morning.

--- config/config.py
+++ config/config.py
@@ -66,7 +66,104 @@ $(document).ready( function () {
 output['fields'].append(dict(
         label='Destinations',
         advice='The destination IDs for this appliance',
-        html='<input type="text" width="80" name="destinations" />'))
+        html='''
+  <!-- jsbin.com/amojef/40 -->
+  <div id="tbdhselectdests">
+    Show destinations within <span class="tbdhmiles" style="color: red"></span> miles:
+    <div class="tbdhslider"></div>
+    <ul class="tbdhdestslist"></ul>
+    <div class="tbdhdestsdrop">Drop destinations here!<ul></ul><div>
+      
+      <input type="hidden" name="destinations" class="tbdhdestinationinp" />
+  </div>
+<script>
+$(document).ready(function () {
+  var lat=trApp.current_appliance.private.lat;
+  var lon=trApp.current_appliance.private.lng;
+  
+  var div = $('#tbdhselectdests');
+  
+  // update the value in the input box
+  var updateValue = function () {
+    var dests = [];
+      
+    div.find('div.tbdhdestsdrop ul li span.tbdhdestid').each(function () {
+        dests.push($(this).text());
+      });
+    
+    div.find('input.tbdhdestinationinp').val(dests.join(','));
+  };
+  
+  div.find('div.tbdhdestsdrop').droppable({
+    drop: function (e, ui) {
+      // check it isn't already dropped.
+      var dests = [];
+      
+      $(this).find('ul li span.tbdhdestid').each(function () {
+        dests.push($(this).text());
+      });
+      
+      if (jQuery.inArray(ui.draggable.find('span.tbdhdestid').text(), dests) == -1) {
+        // add the outer tags back; for some reason $(...).append(ui.draggable) didn't work. This
+        // creates a copy.
+        var li = $('<li>' + ui.draggable.html() + 
+                                  '<a href="#" title="remove destination '+ 
+                                  ui.draggable.find('span.tbdhdestname') + '">X</a>' +
+                                 '</li>');
+    
+        // remove the li if the remove button is clicked
+        li.find('a').click(function (e) {
+            e.preventDefault();
+            $(this).parent('li').remove();
+            updateValue();
+        });
+        
+        // this is a jQuery element for the droppable
+        $(this).find('ul').append(li);
+        
+        // finally update the form element value
+        updateValue();
+      }
+    }
+  });                                     
+  
+  div.find('.tbdhslider').slider({
+    min: 1,
+    max: 40,
+    value: 10 
+  }).bind('slide', function (e, slide) {
+      div.find('span.tbdhmiles').text(slide.value);
+  }).bind('slidechange', function (e, slide) {
+    $.ajax({
+      url: 'http://transitappliance.couchone.com/destinations/_design/geo/_spatiallist/radius/full',
+      data: {
+        // w,s,e,n
+        // 1.5 deg should be enough
+        bbox: [(lon - 0.75), (lat - 0.75), (lon + 0.75), (lat + 0.75)].join(','),
+        radius: slide.value * 1609
+      },
+      dataType: 'jsonp',
+      success: function (data) {
+        if (data.features.length === 0)
+          div.find('ul.tbdhdestslist').html('No destinations within given radius.');
+        else
+          div.find('ul.tbdhdestslist').html('');
+        
+        $.each(data.features, function (ind, feat) {
+          div.find('ul.tbdhdestslist').append('<li><span style="display: none" class="tbdhdestid">' +
+                                              feat.properties._id + '</span><span class="tbdhdestname">' + 
+                                              // it gets converted to a feature twice, once when uploaded and once when downloaded
+                                              feat.properties.properties.name + '</span></li>');
+        });
+        // use sortable so that they snap back if they don't make it to the droppable, and if they
+        // do they don't leave a hole
+        div.find('ul.tbdhdestslist').sortable().disableSelection();
+      }
+    });    
+  });
+});
+</script>
+'''))
 
 output['fields'].append(dict(
         label='Custom stylesheet',
